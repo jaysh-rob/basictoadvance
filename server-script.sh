763d7dde82c948d51e879ab1a5ec6b4a1dad3a6f
jaysh-rob <dvlshukla@gmail.com>
2024-12-25 20:41:04 +0530

    Script has been added to the repo

--- server-script.sh
+++ server-script.sh
@@ -1,15 +1,20 @@
-sudo yum install java-17-amazon-corretto-devel -y
-sudo yum install git -y
-sudo yum install maven -y
+#!/bin/bash
 
-if [ -d "addressbook-v1" ]
+sudo  yum update -y
+sudo yum install java-1.8.0-openjdk-devel -y
+sudo yum install git -y
+sudo yum install maven -y 
+
+
+if [ -d "basictoadvance" ]
 then
-  echo "repo is cloned and exists"
-  cd /home/ec2-user/addressbook-v1
-  git pull origin master
-else
-  git clone https://github.com/preethid/addressbook-v1.git
+    echo "repo is cloned and exists"
+    cd /home/ec2-user/basictoadvance
+    git pull origin b1
+else 
+    git clone https://github.com/jaysh-rob/basictoadvance.git
 fi
 
-cd /home/ec2-user/addressbook-v1
+cd /home/ec2-user/basictoadvance
+
 mvn package
